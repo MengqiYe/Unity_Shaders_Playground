8140945a64b1109fecd426cfbfb6f7f79e924cc5
Mengqi Ye <yemengqi@wondersgroup.com>
2018-07-31 11:01:55 +0800

    Add 0208_PostRendering_Pixelation

---
+++ Assets/Scripts/SetPostRenderingShader.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetPostRenderingShader : MonoBehaviour {
+
+    public Material mat;
+
+    private Camera cam;
+
+	// Use this for initialization
+	void Start () {
+        cam = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnRenderImage(RenderTexture src, RenderTexture dest)
+    {
+        Graphics.Blit(src, dest, mat);
+    }
+}
